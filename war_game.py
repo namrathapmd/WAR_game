ff0b95a279bd063f6358192f00ad1fa2ea89abc8
Namratha <namrathapmd@gmail.com>
2024-02-02 13:08:16 -0600

    added war card game

--- war_game.py
+++ war_game.py
@@ -1,4 +1,4 @@
-# WAR GAME!
+# WAR CARD GAME!
 # The objective of the game is to win all of the cards.
 
 # The deck is divided evenly among the players, giving each a down stack. In unison, each player reveals the top card of their deck—this is a "battle"—and the player with the higher card takes both of the cards played and moves them to their stack.
